843789bae3e9e1dbfe0fc845fd2bd1d08cb6eedd
Aline Brito <nbrito.aline@gmail.com>
2017-10-28 09:18:56 -0200

    Include install script

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+NAME_PROJECT="jaylenw/ElectronTodoApp"
+
+clear
+echo " ------------------ $NAME_PROJECT  ------------------ "
+
+echo "Fetches the list of available updates..."
+sudo apt update
+
+echo "Installing git ..."
+if [ "`dpkg -s git | grep "installed"`" = "" ]; then
+  sudo apt install git
+  echo "git installed!"
+else
+  echo "git is already installed!";
+fi
+
+echo "Installing Node.js..."
+if [ "`dpkg -s nodejs | grep "installed"`" = "" ]; then
+  sudo apt install nodejs
+  echo "Configuring Node.js ..."
+  sudo ln -s /usr/bin/nodejs /usr/bin/node
+  echo "Node.js installed!"
+else
+  echo "Node.js is already installed!";
+fi
+
+echo "Installing npm..."
+if [ "`dpkg -s npm | grep "installed"`" = "" ]; then
+  sudo apt install npm
+  echo "npm installed!"
+else
+  echo "npm is already installed!";
+fi
+
+echo "Installing npm packages..."
+sudo npm install -g npm
+
+echo "Configuring architecture i386..."
+sudo dpkg --add-architecture i386
+
+echo "Configuring dependencies   ..."
+wget -nc https://dl.winehq.org/wine-builds/Release.key
+sudo apt-key add Release.key
+sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+
+echo "Installing mono-complete..."
+if [ "`dpkg -s mono-complete | grep "installed"`" = "" ]; then
+  sudo apt install mono-complete -y
+  echo "mono-complete installed!"
+else
+  echo "mono-complete is already installed!";
+fi
+
+echo "Fetches the list of available updates ..."
+sudo apt update
+
+echo "Installing winehq-devel..."
+if [ "`dpkg -s  winehq-devel | grep "installed"`" = "" ]; then
+  sudo apt install --install-recommends winehq-devel -y
+  echo "winehq-devel installed!"
+else
+  echo "winehq-devel is already installed!";
+fi
+
+echo "Installing npm dependencies...."
+npm install
+
+echo "Finish!"
+exit 0;
